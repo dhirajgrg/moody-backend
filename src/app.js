9b578326db3d3566b0be074bafc31381f82ec2b9
dhirajgrg <gdhiraj030@gmail.com>
2025-08-12 01:50:08 +0300

    final final push to github

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-import songsRoutes from "./routes/songs.route.js"
 import express from "express"
-
 import cors from "cors"
+import songsRoutes from "./routes/songs.route.js"
 
 const app = express()
 app.use(express.json())
+
 const allowedOrigins = [
 	"http://localhost:5173",
 	"http://localhost:4173",
-	process.env.FRONTEND_URL, 
+	process.env.FRONTEND_URL,
 ]
 
 app.use(
