b82736a9f48be8e1ce6d7a417a256c8253d0877f
dhirajgrg <gdhiraj030@gmail.com>
2025-08-11 17:14:57 +0300

    adding vercel json

--- api/index.js
+++
@@ -1,8 +0,0 @@
-import serverless from "serverless-http"
-import app from "../src/app.js"
-import connectDB from "../src/db/db.js"
-
-// Connect to DB once per cold start
-await connectDB()
-
-export const handler = serverless(app)
