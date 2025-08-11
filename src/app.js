1299535d4ca1038ca42abb145f3f4bf8a63532f7
dhirajgrg <gdhiraj030@gmail.com>
2025-08-12 01:32:39 +0300

    final push to github

--- src/app.js
+++ src/app.js
@@ -8,7 +8,20 @@ import cors from "cors"
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+const allowedOrigins = [
+	"http://localhost:5173",
+	"http://localhost:4173",
+	process.env.FRONTEND_URL, 
+]
+
+app.use(
+	cors({
+		origin: allowedOrigins,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	})
+)
+
 app.use("/", songsRoutes)
 
 export default app
